e40b5484854db69910660637ff6208908365a2dc
subin <sblee.b@enki.co.kr>
2025-09-19 11:38:57 +0900

    fix: style 경로

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import './styles/index.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './globals.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
