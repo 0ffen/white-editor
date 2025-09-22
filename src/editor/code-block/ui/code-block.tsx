da08f8ef321f2ef2b8bc9c586d6087f81764eb62
subin <sblee.b@enki.co.kr>
2025-09-22 16:53:29 +0900

    feat: codeblock 구현

---
+++ src/editor/code-block/ui/code-block.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Check, ChevronsUpDown, Copy } from 'lucide-react';
+import {
+  Button,
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components';
+import { cn } from '@/utils';
+import { NodeViewContent, NodeViewWrapper } from '@tiptap/react';
+
+import '@/styles/github-dark.css';
+
+interface Props {
+  node: {
+    attrs: { language: string };
+  };
+  updateAttributes: (attrs: { language: string }) => void;
+  extension: {
+    options: {
+      lowlight: {
+        listLanguages: () => string[];
+      };
+    };
+  };
+}
+
+export const CodeBlock = ({
+  node: {
+    attrs: { language: defaultLanguage },
+  },
+  updateAttributes,
+  extension,
+}: Props) => {
+  const languageOptions = extension.options.lowlight.listLanguages().map((lang) => ({
+    label: lang,
+    value: lang,
+  }));
+
+  const [isCopied, setIsCopied] = React.useState<boolean>(false);
+
+  const handleCopy = React.useCallback(() => {
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <NodeViewWrapper>
+      <pre className='hljs'>
+        <div className='flex w-full justify-between'>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant='ghost'
+                role='combobox'
+                className={cn(
+                  'text-muted-foreground hover:text-muted-foreground w-fit justify-between hover:bg-transparent'
+                )}
+              >
+                {defaultLanguage || 'Select language'}
+                <ChevronsUpDown className='opacity-50' />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className='w-[180px] p-0' align='start'>
+              <Command>
+                <CommandInput placeholder='Search language' className='h-9' />
+                <CommandList>
+                  <CommandEmpty>No language found</CommandEmpty>
+                  <CommandGroup>
+                    {languageOptions.map((language) => (
+                      <CommandItem
+                        value={language.label}
+                        key={language.value}
+                        onSelect={() => {
+                          updateAttributes({ language: language.value });
+                        }}
+                      >
+                        {language.label}
+                        <Check
+                          className={cn('ml-auto', language.value === defaultLanguage ? 'opacity-100' : 'opacity-0')}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+          <Button
+            onClick={handleCopy}
+            variant='ghost'
+            className='hover:text-primary w-fit hover:cursor-pointer hover:bg-stone-800'
+          >
+            {isCopied ? (
+              <Check className='text-muted-foreground size-4' />
+            ) : (
+              <Copy className='text-muted-foreground size-4' />
+            )}
+          </Button>
+        </div>
+        <code>
+          <NodeViewContent as='div' />
+        </code>
+      </pre>
+    </NodeViewWrapper>
+  );
+};
