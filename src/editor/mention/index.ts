db54248fe7daa32878e2e43db6a32a7d5814046d
subin <sblee.b@enki.co.kr>
2025-09-23 17:57:05 +0900

    chore: 폴더구조 개선

--- src/editor/mention/index.ts
+++ src/editor/mention/index.ts
@@ -1,3 +1,4 @@
-export * from './node/mention-suggestion';
+export * from './util/mention-suggestion';
+export * from './util/mention-node';
 export * from './ui/mention-list';
 export * from './type/mention.type';
