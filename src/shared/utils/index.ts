2cba5426264cc5767eca5987b589042be501c446
subin <sblee.b@enki.co.kr>
2025-09-30 17:02:35 +0900

    chore: 파일명 컨벤션에 맞게 수정

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -3,5 +3,5 @@ export * from './update-position';
 export * from './list-config';
 export * from './theme';
 export * from './extensions';
-export * from './getHtmlContent';
-export * from './createEmptyContent';
+export * from './get-html-content';
+export * from './create-empty-content';
