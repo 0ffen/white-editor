36d502699f1b63254cdf0d375ef2260c8583193d
subin <sblee.b@enki.co.kr>
2026-08-20 11:48:40 +0900

    fix: caption copy 허용 (#69)

--- src/white-editor/nodes/image/ui/node/caption.tsx
+++ src/white-editor/nodes/image/ui/node/caption.tsx
@@ -8,6 +8,11 @@ interface ImageCaptionProps {
 
 /**
  * 이미지 캡션 컴포넌트
+ *
+ * 캡션은 실제 텍스트 노드로 렌더링한다. (CSS `content: attr()` 의사 요소는 브라우저에 따라
+ * 클립보드에 포함되지 않아 뷰어에서 복사되지 않는다.)
+ * `data-image-caption`은 붙여넣기 시 캡션이 중복되지 않도록 제거하는 마커로 사용된다.
+ * @see resizable-image.ts `transformPastedHTML`
  */
 export const ImageCaption: React.FC<ImageCaptionProps> = (props: ImageCaptionProps) => {
   const { caption, imageWidth, className } = props;
@@ -20,10 +25,12 @@ export const ImageCaption: React.FC<ImageCaptionProps> = (props: ImageCaptionPro
     <div
       data-image-caption={caption}
       className={cn(
-        'image-caption-pseudo we:text-text-sub we:break-all we:mt-2 we:text-center we:text-xs we:whitespace-pre-wrap',
+        'we:text-text-sub we:break-all we:mt-2 we:text-center we:text-xs we:whitespace-pre-wrap',
         className
       )}
       style={captionStyle}
-    />
+    >
+      {caption}
+    </div>
   );
 };
